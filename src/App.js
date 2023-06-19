787597cfc00023dcfd8160f35ca724be35ab035f
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-19 17:59:27 -0400

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <a href="/">
-            Rocket
-          </a>
-        </nav>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/rockets" element={<h1>Rockets</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
