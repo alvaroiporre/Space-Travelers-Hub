f1a58cd78fafc115498634c13e351561be6148dc
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-19 18:05:50 -0400

    Add NavLink routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import {
   BrowserRouter, Route, Routes,
 } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/rockets" element={<h1>Rockets</h1>} />
+          <Route path="/missions" element={<h1>Missions</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
