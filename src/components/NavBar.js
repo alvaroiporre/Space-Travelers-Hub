6e0a186c93bddf985d4f94a6cc76ddb68666af84
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-20 19:29:56 -0400

    Add navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,12 @@ import logo from '../assets/planet.png';
 
 function NavBar() {
   return (
-    <nav>
-      <img src={logo} alt="logo" className="logo" />
-      <ul>
+    <nav className="navbar">
+      <h1>
+        <img src={logo} alt="Space Travellers Logo" className="logo" />
+        Space Travelers&apos; Hub
+      </h1>
+      <ul className="nav-menu">
         <li><NavLink exact to="/" activeclassname="active">Home</NavLink></li>
         <li><NavLink to="/rockets" activeclassname="active">Rockets</NavLink></li>
         <li><NavLink to="/missions" activeclassname="active">Missions</NavLink></li>
