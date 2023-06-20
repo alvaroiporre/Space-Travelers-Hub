a349bb03ffba31e6714eeb2291a41389fd0cb806
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-20 11:58:51 -0600

    Create a directory for all Redux state slice files and style active class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const activeStyle = {
+    textDecoration: 'underline',
+  };
+
   return (
     <nav>
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/rockets">Rockets</NavLink></li>
-        <li><NavLink to="/missions">Missions</NavLink></li>
+        <li><NavLink exact to="/" activeClassName="active" style={activeStyle}>Home</NavLink></li>
+        <li><NavLink to="/rockets" activeClassName="active" style={activeStyle}>Rockets</NavLink></li>
+        <li><NavLink to="/missions" activeClassName="active" style={activeStyle}>Missions</NavLink></li>
       </ul>
     </nav>
   );
 }
+
 
 export default NavBar;
