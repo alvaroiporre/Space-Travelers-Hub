02a5376b5b6637aee5fbcc4a836a57054debcda2
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-20 15:33:28 -0600

    Fix render issue fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import './assets/index.css';
-import { Provider } from 'react-redux';
 import store from './redux/store';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
