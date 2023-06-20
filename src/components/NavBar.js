3e5c4c1e38179a6e4f12870aea13d57309d82df1
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-20 12:25:24 -0600

    Add logo app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
-  const activeStyle = {
-    textDecoration: 'underline',
-  };
-
   return (
     <nav>
       <ul>
-        <li><NavLink exact to="/" activeClassName="active" style={activeStyle}>Home</NavLink></li>
-        <li><NavLink to="/rockets" activeClassName="active" style={activeStyle}>Rockets</NavLink></li>
-        <li><NavLink to="/missions" activeClassName="active" style={activeStyle}>Missions</NavLink></li>
+        <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+        <li><NavLink to="/rockets" activeClassName="active">Rockets</NavLink></li>
+        <li><NavLink to="/missions" activeClassName="active">Missions</NavLink></li>
       </ul>
     </nav>
   );
