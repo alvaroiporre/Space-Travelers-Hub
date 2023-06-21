32b2c5788cbaceb1119b1bc127d7cd3c606d6d57
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-21 11:40:45 -0400

    Add conditional components

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -17,7 +17,7 @@ const Mission = ({ mission }) => {
     <>
       <h3 className="rocket-name">{mission.name}</h3>
       <p className="rocket-description">{mission.description}</p>
-      <p>NOT A MEMBER</p>
+      <p>{mission.reserved ? "Active Member" : "NOT A MEMBER"}</p>
       {
         mission.reserved
           ? <button type="button" className="reserveBttn" onClick={handleLeave}>Leave Mission</button>
