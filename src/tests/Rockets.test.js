70b1684fbe3a5356ddb7d172564da49198c55f42
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-22 18:48:44 -0400

    Add mission-redux test

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import Rockets from './Rockets';
+import Rockets from '../components/Rockets';
 
 const mockStore = configureStore([]);
 
