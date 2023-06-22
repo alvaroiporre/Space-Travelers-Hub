36ea2f655883bbb9bc7e7966427d01ac593e2817
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-22 10:41:49 -0600

    Add rockets reservations to my profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,24 +3,37 @@ import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
   return (
     <section className="my-profile-container">
-      <article>
-        <h2>My Rockets</h2>
-      </article>
       <article>
         <h2>My Missions</h2>
         {
           missions
-        && (
-        <ul className="missions-reserved">
-          {
-            missions.map((mission) => (
-              mission.reserved && <li key={mission.id}><b>{mission.name}</b></li>
-            ))
-          }
-        </ul>
-        )
+          && (
+            <ul className="missions-reserved">
+              {
+                missions.map((mission) => (
+                  mission.reserved && <li key={mission.id}><b>{mission.name}</b></li>
+                ))
+              }
+            </ul>
+          )
+        }
+      </article>
+      <article>
+        <h2>My Rockets</h2>
+        {
+          rockets
+          && (
+            <ul className="rockets-reserved">
+              {
+                rockets.map((rockets) => (
+                  rockets.reserved && <li key={rockets.id}><b>{rockets.name}</b></li>
+                ))
+              }
+            </ul>
+          )
         }
       </article>
     </section>
