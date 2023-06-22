230c58f4ec604ffd1306ee254570129f9af37114
Jrmcuervo <jrmcuervom@gmail.com>
2023-06-22 11:05:51 -0600

    Use filter for reservations

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,37 +4,35 @@ import { useSelector } from 'react-redux';
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
   const rockets = useSelector((state) => state.rockets.rockets);
+
+  const reservedMissions = missions.filter((mission) => mission.reserved);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
     <section className="my-profile-container">
       <article>
         <h2>My Missions</h2>
-        {
-          missions
-          && (
-            <ul className="missions-reserved">
-              {
-                missions.map((mission) => (
-                  mission.reserved && <li key={mission.id}><b>{mission.name}</b></li>
-                ))
-              }
-            </ul>
-          )
-        }
+        {reservedMissions.length > 0 ? (
+          <ul className="missions-reserved">
+            {reservedMissions.map((mission) => (
+              <li key={mission.id}><b>{mission.name}</b></li>
+            ))}
+          </ul>
+        ) : (
+          <p>No missions reserved</p>
+        )}
       </article>
       <article>
         <h2>My Rockets</h2>
-        {
-          rockets
-          && (
-            <ul className="rockets-reserved">
-              {
-                rockets.map((rockets) => (
-                  rockets.reserved && <li key={rockets.id}><b>{rockets.name}</b></li>
-                ))
-              }
-            </ul>
-          )
-        }
+        {reservedRockets.length > 0 ? (
+          <ul className="rockets-reserved">
+            {reservedRockets.map((rocket) => (
+              <li key={rocket.id}><b>{rocket.name}</b></li>
+            ))}
+          </ul>
+        ) : (
+          <p>No rockets reserved</p>
+        )}
       </article>
     </section>
   );
